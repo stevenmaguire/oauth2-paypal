6781e9cf1483c78d0f71b4600055c48c6f9b24a9
Steven Maguire <stevenmaguire@gmail.com>
2017-01-25 20:35:37 -0600

    Update base package version

--- src/Provider/Paypal.php
+++ src/Provider/Paypal.php
@@ -118,7 +118,7 @@ class Paypal extends AbstractProvider
      *
      * @param object $response
      * @param AccessToken $token
-     * @return League\OAuth2\Client\Provider\ResourceOwnerInterface
+     * @return PaypalResourceOwner
      */
     protected function createResourceOwner(array $response, AccessToken $token)
     {
